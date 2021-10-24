e9b2751c2fbffc8f438a9dce4f427d49712a71f2
Eta23 <aneta.milenkovic@hotmail.com>
2021-10-25 00:08:35 +0200

    feature/SLE-001 Modified login steps file, added assert

--- stepDefinitions/login_steps.js
+++ stepDefinitions/login_steps.js
@@ -1,7 +1,10 @@
 const { Given, When, Then } = require('cucumber');
 
-const LoginPage = require('../support/pages/login_page');
+const HomePage = require('../support/pages/home_page');
+import assert from 'assert';
+//var assert = require('assert');
 
+const LoginPage = require('../support/pages/login_page');
 
 
 Given(/^User is on Landing page and navigate to Login page$/, function(){
@@ -16,8 +19,10 @@ When(/^User click on Login button$/, function(){
     LoginPage.clickOnLoginButton()
 });
 
-Then(/^User is successifully logged into Login page$/, function(){
+Then(/^User is successifully logged into Home page$/, function(){
     var strUrl = browser.getUrl();
-    console.log("URL is : "=strUrl);
+    console.log("URL is : " + strUrl);
+// expected, actual
+    assert.equal(strUrl, "https");
 });
 
