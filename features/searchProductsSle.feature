7a5939245ac3ac24c45ef40dac96ac3f7aca5772
Eta23 <aneta.milenkovic@hotmail.com>
2021-10-25 15:20:10 +0200

    SLE-001 Finish commit, login test case done

--- features/searchProductsSle.feature
+++ features/searchProductsSle.feature
@@ -3,7 +3,7 @@ Feature: Search products
 
     Background: 
         Given User is on SLE Landing page
-        //User doesn't have to be logged into SLE account
+        #User doesn't have to be logged into SLE account in order to search products
 
     Scenario: User can search products via “Search product” bar at the top of the page
         When User type word to search products in “Search product” bar at the top of the SLE Home page
--- stepDefinitions/login_steps.js
+++ stepDefinitions/login_steps.js
@@ -1,9 +1,8 @@
+
 const { Given, When, Then } = require('@cucumber/cucumber');
-
-const HomePage = require('../support/pages/home_page');
-var assert = require('assert');
-
 const LoginPage = require('../support/pages/login_page');
+const HomePage = require('../support/pages/home_page');
+
 
 
 Given(/^User is on Landing page and navigate to Login page$/, function(){
@@ -19,9 +18,6 @@ When(/^User click on Login button$/, function(){
 });
 
 Then(/^User is successifully logged into Home page$/, function(){
-    var strUrl = browser.getUrl();
-    console.log("URL is : " + strUrl);
-// expected, actual
-    assert.equal(strUrl, "https");
+    LoginPage.confirmHomePage()
 });
 
